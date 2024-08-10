324844df023434cb0d7325a6fd686be5d6e5b0bc
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-08-10 22:38:34 +0700

    feat(model): add all model requirement

--- database/migrations/2024_08_10_061300_create_products_table.php
+++ database/migrations/2024_08_10_061300_create_products_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->string('category_product_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('price');
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -29,8 +28,8 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2024_08_10_062235_create_category_products_table.php
+++ database/migrations/2024_08_10_062235_create_category_products_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('category_products', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,8 +25,8 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('category_products');
     }
 };
